2c27d7491863c7068c7f3149817a27e04024f0b3
Sarit Plewma <splewma2008@hotmail.com>
2020-11-20 15:40:06 +0700

    add db in index.js

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,20 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const db = require('./models')
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// app.use("/partners",);
 
 app.listen(process.env.PORT, () => {
    console.log(`Server is running at PORT ${process.env.PORT} `);
-})
+});
+
+db.sequelize.sync({ force: false }).then(() => {
+   console.log("Database connected.")
+}).catch(err => {
+   console.log(err);
+});
