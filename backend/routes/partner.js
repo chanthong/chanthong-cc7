25cd003fdfeae208cf5faa213b2f89464fc773cb
Sarit Plewma <splewma2008@hotmail.com>
2020-11-21 11:24:40 +0700

    Sixth commit again again

--- backend/routes/partner.js
+++ backend/routes/partner.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 
 const {register} = require("../controllers/partner");
-=======
-const db = require('../models');
-const { register } = require("../controllers/partner");
->>>>>>> 865b2df4a0b74b16323bfc0de608453fb686421b
 const router = require("express").Router();
 
 router.post("/register", register);
