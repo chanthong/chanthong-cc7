d858be2fbf58ef4f1242aaf789b76d082da31e07
jarabeenoi <nongnat61@gmail.com>
2020-11-23 09:36:24 +0700

    first setup

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,26 @@
 require("dotenv").config();
 const express = require("express");
-const app = express();
+const cors = require("cors");
+const db = require('./models');
+
 const partnerRoutes = require("./routes/partner");
-const db = require('./models')
+const userRoutes = require("./routes/user");
 
+const app = express();
 
-
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use("/partners", partnerRoutes);
 
-app.use("partners", partnerRoutes);
+app.use("/partners", partnerRoutes);
+app.use("/users", userRoutes);
 
 app.listen(process.env.PORT, () => {
    console.log(`Server is running at PORT ${process.env.PORT} `);
 });
 
 db.sequelize.sync({ force: false}).then(() => {
-   console.log("Database connected.")
+   console.log("Database connected.");
 }).catch(err => {
    console.log(err);
 });
