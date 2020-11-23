a2f83b43bf1763793a48be8f9d6d94f86c345983
Sarit Plewma <splewma2008@hotmail.com>
2020-11-23 20:57:20 +0700

    add partner form

--- frontend/src/containers/pages/PartnerProfile/PartnerProfile.js
+++ frontend/src/containers/pages/PartnerProfile/PartnerProfile.js
@@ -3,8 +3,28 @@ import React from 'react';
 function PartnerProfile() {
     return (
         <div className="wrapper">
-            <div>
-
+            <div className="title">
+                Partner Form
+            </div>
+            <div className="form">
+                <div className="inputfield">
+                    <label>Restaurant Name</label>
+                </div>
+                <div className="inputfield">
+                    <label>User Name</label>
+                </div>
+                <div className="inputfield">
+                    <label>Password</label>
+                </div>
+                <div className="inputfield">
+                    <label>Confirm Password</label>
+                </div>
+                <div className="inputfield">
+                    <label>Email Address</label>
+                </div>
+                <div className="inputfield">
+                    <label>Phone Number</label>
+                </div>
             </div>
         </div>
     )
