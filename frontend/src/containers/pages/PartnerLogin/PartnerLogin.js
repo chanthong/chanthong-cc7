626270b67a323326ecf264670ef281233cf29d48
Sarit Plewma <splewma2008@hotmail.com>
2020-11-23 11:24:49 +0700

    add file partner_login

--- frontend/src/containers/pages/PartnerLogin/PartnerLogin.js
+++ frontend/src/containers/pages/PartnerLogin/PartnerLogin.js
@@ -3,9 +3,9 @@ import React from 'react'
 function PartnerLogin() {
     return (
         <div>
-
+            PartnerLogin
         </div>
     )
 }
 
-export default PartnerLogin
+export default PartnerLogin;
