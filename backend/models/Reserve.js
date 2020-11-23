4b070a9fc6f01b868f7dddeb08d66de66f767863
BlueSphinx <thatana.b@gmail.com>
2020-11-23 13:10:34 +0700

    Second Reservation

--- backend/models/Reserve.js
+++ backend/models/Reserve.js
@@ -1,15 +1,53 @@
 module.exports = (sequelize, DataTypes) => {
-    const Reserve = sequelize.define("Reserve", {
-      reserve: DataTypes.STRING
-    }, {
+  const Reserve = sequelize.define("Reserve", {
+    reserve: DataTypes.STRING,
+    date: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    time: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    number_guest: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    reserve_code: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    note_comment: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    review_star: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    review: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    reserve_status: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
+  },
+
+    {
       tableName: "reserves",
-      timestamps: false
+      timestamps: false,
     });
-  
-    Reserve.associate = models => {
-      Reserve.belongsTo(models.User, { foreignKey: "user_id" });
-      Reserve.belongsTo(models.Partner, { foreignKey: "partner_id" });
-    };
-  
-    return Reserve;
-  }
+
+  Reserve.associate = models => {
+    Reserve.belongsTo(models.User, { foreignKey: "user_id" });
+    Reserve.belongsTo(models.Partner, { foreignKey: "partner_id" });
+  };
+
+  return Reserve;
+}
