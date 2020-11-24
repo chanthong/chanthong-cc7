2b556d9ca077bc7c2a356126954e61afa114a754
Sarit <51039455+Gigaman0101@users.noreply.github.com>
2020-11-24 14:55:42 +0700

    create component Chanthong role

---
+++ frontend/src/containers/pages/ChanthongRole/ChanthongRole.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import ResturantCard2 from '../../../components/ResturantCard/ResturantCard2/ResturantCard2';
+
+function ChanthongRole() {
+    const [card, setCard] = useState([]);
+
+    const fetchPartners = async () => {
+        const res = await axios.get("http://localhost:5555/partners/");
+        setCard(res.data.partners);
+    }
+
+    useEffect(() => {
+        fetchPartners();
+    }, []);
+
+    return (
+        <div>
+            <h1 style={{ textAlign: "start", fontSize: "24px", fontStyle: "italic" }}>Recommend by Chanthong</h1>
+            <div style={{ display: "flex" }}>
+                {card.map((item) => <ResturantCard2 card={item} />)}
+            </div>
+        </div>
+    )
+}
+
+export default ChanthongRole
--- frontend/src/containers/pages/Home/Home.js
+++ frontend/src/containers/pages/Home/Home.js
@@ -1,33 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import Navbar from '../../../components/Navbar/Navbar';
 // import ResturantCard from '../../../components/ResturantCard/ResturantCard';
-import ResturantCard2 from '../../../components/ResturantCard/ResturantCard2/ResturantCard2';
-import axios from 'axios';
+import ChanthongRole from '../ChanthongRole/ChanthongRole';
 
 function Home({ role, setRole }) {
-   const [card, setCard] = useState([]);
 
-   const fetchPartners = async () => {
-      const res = await axios.get("http://localhost:5555/partners/");
-      setCard(res.data.partners);
-   }
-
-   useEffect(() => {
-      fetchPartners();
-   }, []);
-
-<<<<<<< HEAD
    return (
-      <div style={{ display: 'flex' }}>
+      <div >
          <Navbar role={role} setRole={setRole} />
-         {/* <ResturantCard /> */}
-         {card.map((item) => <ResturantCard2 card={item} />)}
-=======
-function Home({ role, setRole }) {
-   return (
-      <div>
-         <Navbar role={role} setRole={setRole} />
->>>>>>> dev
+         <ChanthongRole />
       </div>
    )
 }
