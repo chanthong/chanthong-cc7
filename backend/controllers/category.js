618778c8f8faa446c938c26a6d63fc5726aa29db
Sarit <51039455+Gigaman0101@users.noreply.github.com>
2020-11-26 14:17:53 +0700

    fix attribute Category

--- backend/controllers/category.js
+++ backend/controllers/category.js
@@ -2,22 +2,21 @@ const db = require("../models");
 
 const createCategory = async (req, res) => {
     try {
-        const { type_food } = req.body;
+        const { type_Category, type_Restaurant } = req.body;
         const newCategory = await db.Category.create({
-            type_food,
-            partner_id: req.user.id
+            type_Category,
+            type_Restaurant,
         });
 
         res.status(201).send(newCategory);
     } catch (err) {
         res.status(500).send({ messages: err.message });
-    }
+    };
 };
 
 const getAllCategories = async (req, res) => {
     try {
-        const allCategories = await db.Category.findAll({
-        });
+        const allCategories = await db.Category.findAll({});
         res.status(200).send(allCategories);
     } catch (err) {
         res.status(500).send({ messages: err.message });
