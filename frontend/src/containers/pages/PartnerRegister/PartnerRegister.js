25090ad9c192a5888d093e62da1ad92ece96b7e1
BlueSphinx <thatana.b@gmail.com>
2020-11-26 14:03:11 +0700

    partner register second

--- frontend/src/containers/pages/PartnerRegister/PartnerRegister.js
+++ frontend/src/containers/pages/PartnerRegister/PartnerRegister.js
@@ -5,6 +5,7 @@ import { BASE_BACKEND_URL } from '../../../config/constants';
 import { withRouter } from "react-router-dom";
 import { notification } from 'antd';
 import PlateImg from '../../uploadfile/dishelement.png'
+import RegisterImg from '../../uploadfile/register.png'
 
 function PartnerRegister(props) {
     const [username, setUsername] = useState("");
@@ -13,8 +14,12 @@ function PartnerRegister(props) {
     const [phone_number, setPhone_number] = useState("");
     const [email_address, setEmail_address] = useState("");
     const [price_range, setPrice_range] = useState("");
-    const [location, setLocation] = useState("");
+    const [address, setAddress] = useState("");
+    const [district, setDistrict] = useState("");
+    const [province, setProvince] = useState("");
     const [partners_picture, setPartners_picture] = useState("");
+    const [category1, setCategory1] = useState("");
+    const [category2, setCategory2] = useState("");
 
     const usernameHandler = (e) => {
         setUsername(e.target.value);
@@ -34,16 +39,28 @@ function PartnerRegister(props) {
     const price_rangeHandler = (e) => {
         setPrice_range(e.target.value);
     };
-    const locationHandler = (e) => {
-        setLocation(e.target.value);
+    const addressHandler = (e) => {
+        setAddress(e.target.value);
+    };
+    const districtHandler = (e) => {
+        setDistrict(e.target.value);
+    };
+    const provinceHandler = (e) => {
+        setProvince(e.target.value);
     };
     const partners_pictureHandler = (e) => {
         setPartners_picture(e.target.value);
     };
+    const category1Handler = (e) => {
+        setCategory1(e.target.value);
+    };
+    const category2Handler = (e) => {
+        setCategory2(e.target.value);
+    };
 
     const onFinish = () => {
         console.log("Comein");
-        axios.post("/partners/register", { username, password, restaurant_name, email_address, phone_number, price_range, location, partners_picture })
+        axios.post("/partners/register", { username, password, restaurant_name, email_address, phone_number, price_range, address, district, province, partners_picture, category1, category2 })
             .then(res => {
                 notification.success({
                     description: "Signup successfully"
@@ -125,55 +142,100 @@ function PartnerRegister(props) {
                 <div className="innerPlatePartner"></div>
             </div>
 
+            <div className="welcome font-lobster" style={{fontSize: "180%", top: "40px"}}>Welcome to</div>
+            <div className="chanthong font-lobster" style={{fontSize: "280%", top: "80px", left: "200px"}}>Chanthong Community</div>
+
             {/* 1st column */}
             <div>
                 <img className="plateLeftPartner" src={PlateImg} alt="plate" style={{ left: "57px", top: "200px" }}></img>
             </div>
             <div className="usernamePartner font-mitr" style={{left: "0px", top: "180px"}}>Username:</div>
-            <div className="usernameTabPartner font-mitr" style={{left: "60px", top: "200px"}}>User</div>
+            <div className="usernameTabPartner font-mitr" style={{left: "60px", top: "200px"}}>{username}</div>
 
             <div>
                 <img className="plateLeftPartner" src={PlateImg} alt="plate" style={{ left: "57px", top: "270px" }}></img>
             </div>
             <div className="usernamePartner font-mitr" style={{left: "0px", top: "250px"}}>Password:</div>
-            <div className="usernameTabPartner font-mitr" style={{left: "60px", top: "270px"}}>Password</div>
+            <div className="usernameTabPartner font-mitr" style={{left: "60px", top: "270px"}}>{password}</div>
 
             <div>
                 <img className="plateLeftPartner" src={PlateImg} alt="plate" style={{ left: "57px", top: "340px" }}></img>
             </div>
             <div className="usernamePartner font-mitr" style={{left: "32px", top: "320px"}}>Re-enter Password:</div>
-            <div className="usernameTabPartner font-mitr" style={{left: "60px", top: "340px"}}>Re-enter Password</div>
+            <div className="usernameTabPartner font-mitr" style={{left: "60px", top: "340px"}}>{password}</div>
 
             <div>
                 <img className="plateLeftPartner" src={PlateImg} alt="plate" style={{ left: "57px", top: "410px" }}></img>
             </div>
             <div className="usernamePartner font-mitr" style={{left: "15px", top: "390px"}}>Email Address:</div>
-            <div className="usernameTabPartner font-mitr" style={{left: "60px", top: "410px"}}>Email</div>
+            <div className="usernameTabPartner font-mitr" style={{left: "60px", top: "410px"}}>{email_address}</div>
 
             <div>
                 <img className="plateLeftPartner" src={PlateImg} alt="plate" style={{ left: "57px", top: "480px" }}></img>
             </div>
             <div className="usernamePartner font-mitr" style={{left: "36px", top: "460px"}}>Name of Restaurant:</div>
-            <div className="usernameTabPartner font-mitr" style={{left: "60px", top: "480px"}}>Some restaurant</div>
+            <div className="usernameTabPartner font-mitr" style={{left: "60px", top: "480px"}}>{restaurant_name}</div>
 
             {/* 2nd column */}
             <div>
-                <img className="plateLeftPartner" src={PlateImg} alt="plate" style={{ left: "457px", top: "200px" }}></img>
+                <img className="plateLeftPartner" src={PlateImg} alt="plate" style={{ left: "327px", top: "200px" }}></img>
             </div>
-            <div className="usernamePartner font-mitr" style={{left: "436px", top: "180px"}}>Name of Restaurant:</div>
-            <div className="usernameTabPartner font-mitr" style={{left: "460px", top: "200px"}}>Some restaurant</div>
+            <div className="usernamePartner font-mitr" style={{left: "285px", top: "180px"}}>Phone Number:</div>
+            <div className="usernameTabPartner font-mitr" style={{left: "330px", top: "200px"}}>{phone_number}</div>
 
             <div>
-                <img className="plateLeftPartner" src={PlateImg} alt="plate" style={{ left: "457px", top: "270px" }}></img>
+                <img className="plateLeftPartner" src={PlateImg} alt="plate" style={{ left: "327px", top: "270px" }}></img>
             </div>
-            <div className="usernamePartner font-mitr" style={{left: "392px", top: "250px"}}>Address:</div>
-            <div className="usernameTabPartner font-mitr" style={{left: "460px", top: "270px"}}>Address</div>
+            <div className="usernamePartner font-mitr" style={{left: "261px", top: "250px"}}>Address:</div>
+    <div className="usernameTabPartner font-mitr" style={{left: "330px", top: "270px"}}>{address}</div>
 
             <div>
-                <img className="plateLeftPartner" src={PlateImg} alt="plate" style={{ left: "457px", top: "340px" }}></img>
+                <img className="plateLeftPartner" src={PlateImg} alt="plate" style={{ left: "327px", top: "340px" }}></img>
             </div>
-            <div className="usernamePartner font-mitr" style={{left: "392px", top: "320px"}}>District:</div>
-            <div className="usernameTabPartner font-mitr" style={{left: "460px", top: "340px"}}>District</div>
+            <div className="usernamePartner font-mitr" style={{left: "258px", top: "320px"}}>District:</div>
+    <div className="usernameTabPartner3 font-mitr" style={{left: "330px", top: "340px"}}>{district}</div>
+
+            <div>
+                <img className="plateLeftPartner" src={PlateImg} alt="plate" style={{ left: "552px", top: "340px" }}></img>
+            </div>
+            <div className="usernamePartner font-mitr" style={{left: "490px", top: "320px"}}>Province:</div>
+            <div className="usernameTabPartner3 font-mitr" style={{left: "555px", top: "340px"}}>{province}</div>
+
+            <div>
+                <img className="plateLeftPartner" src={PlateImg} alt="plate" style={{ left: "327px", top: "410px" }}></img>
+            </div>
+            <div className="usernamePartner font-mitr" style={{left: "265px", top: "390px"}}>Category:</div>
+            <div className="usernameTabPartner3 font-mitr" style={{left: "330px", top: "410px"}}>{category1}</div>
+
+            <div>
+                <img className="plateLeftPartner" src={PlateImg} alt="plate" style={{ left: "552px", top: "410px" }}></img>
+            </div>
+            <div className="usernamePartner font-mitr" style={{left: "490px", top: "390px"}}>Category:</div>
+            <div className="usernameTabPartner3 font-mitr" style={{left: "555px", top: "410px"}}>{category2}</div>
+
+            <div>
+                <img className="plateLeftPartner" src={PlateImg} alt="plate" style={{ left: "327px", top: "480px" }}></img>
+            </div>
+            <div className="usernamePartner font-mitr" style={{left: "290px", top: "460px"}}>Price per Person:</div>
+            <div className="usernameTabPartner3 font-mitr" style={{left: "330px", top: "480px"}}>{price_range}</div>
+
+            <div>
+                <img className="plateLeftPartner" src={PlateImg} alt="plate" style={{ left: "552px", top: "480px" }}></img>
+            </div>
+            <div className="usernamePartner font-mitr" style={{left: "545px", top: "460px"}}>Upload your best photos:</div>
+    <div className="usernameTabPartner2 font-mitr" style={{left: "555px", top: "480px"}}>{partners_picture}</div>
+            <div className="usernameTabPartner4 font-mitr" style={{left: "780px", top: "480px", color: "#D85B55", fontWeight: "bold"}}>Browse</div>
+
+            {/* Column 3 */}
+            <div>
+                <img className="plateLeftPartner" src={PlateImg} alt="plate" style={{ left: "790px", top: "270px" }}></img>
+            </div>
+            <div className="usernamePartner font-mitr" style={{left: "735px", top: "250px"}}>Description:</div>
+            <div className="usernameTabPartner5 font-mitr" style={{left: "795px", top: "270px"}}>Description</div>
+
+            <div>
+                <img className="registerImage" src={RegisterImg} alt="plate" style={{ left: "960px", top: "470px" }}></img>
+            </div>
         </div>
     )
 }
