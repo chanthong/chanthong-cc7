e7232130c2ed0e95a7d76842fc075a870dc9f3a9
Sarit <51039455+Gigaman0101@users.noreply.github.com>
2020-12-02 12:58:41 +0700

    modify partner routes backend

--- backend/routes/partner.js
+++ backend/routes/partner.js
@@ -5,9 +5,9 @@ const router = require("express").Router();
 
 const auth = passport.authenticate("jwt-auth", { session: false });
 
+router.get("/district", getPartnersByDistrict);
 router.get("/:id", getPartnerById);
 router.get("/", getPartners);
-router.get("/", getPartnersByDistrict);
 router.post("/register", register);
 router.post("/login", login);
 router.delete("/:id", deletePartner);
--- frontend/src/containers/pages/Home/Home.js
+++ frontend/src/containers/pages/Home/Home.js
@@ -4,6 +4,7 @@ import TopHeader from '../../../components/TopHeader/TopHeader';
 // import axios from '../../../config/axios';
 // import { BASE_BACKEND_URL } from '../../../config/constants'
 import ChanthongRole from '../ChanthongRole/ChanthongRole';
+import BottomHeader from '../../../components/BottomHeader/BottomHeader';
 
 
 function Home({ role, setRole }) {
@@ -11,8 +12,8 @@ function Home({ role, setRole }) {
    return (
       <>
          {role === "GUEST" && <TopHeader />}
-         {role === "USER" && <BottomHeader role={role} setRole={setRole} />}         
-        
+         {role === "USER" && <BottomHeader role={role} setRole={setRole} />}
+
          <ChanthongRole />
       </>
    )
