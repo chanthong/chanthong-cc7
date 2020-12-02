22072d21e6a15f564c17ef25f102e6355557df21
BlueSphinx <thatana.b@gmail.com>
2020-12-02 15:42:38 +0700

    Modified Nav Bar and Plate

--- frontend/src/containers/pages/ChanthongRole/ChanthongRole.js
+++ frontend/src/containers/pages/ChanthongRole/ChanthongRole.js
@@ -26,11 +26,13 @@ function ChanthongRole() {
     }, []);
 
     return (
-        <div>
-            <h1 style={{ textAlign: "start", fontSize: "24px", fontStyle: "italic" }}>Recommend by Chanthong</h1>
-            <div style={{ display: "flex", justifyContent: "center" }}>
-                {card.map((item) => <ResturantCard2 item={item} />)}
-                {/* {card.map((item) => <ResturantCard item={item} />)} */}
+        <div style={{ display: 'flex', flexDirection: "column" }}>
+            <h1 style={{ textAlign: "start", fontSize: "24px", fontStyle: "italic" }}>Recommend by 5555</h1>
+            <div style={{ display: "flex" }}>
+                <div style={{ display: "flex", justifyContent: "center" }}>
+                    {card.map((item) => <ResturantCard2 item={item} />)}
+                    {/* {card.map((item) => <ResturantCard item={item} />)} */}
+                </div>
             </div>
         </div>
     )
